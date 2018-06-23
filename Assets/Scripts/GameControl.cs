a229e1bd3fdf9278f112eb586919df389ec28423
ShashankSharma17 <38305189+ShashankSharma17@users.noreply.github.com>
2018-06-23 17:52:23 +0530

    Version 1.0 (Clean Built)

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+	public GameObject gameOvertext; 
+	public bool gameOver = false; 
+	public static GameControl instance;
+	public float scrollSpeed = -1.5f;
+	public Text scoreText; 
+	private int score = 0; 
+
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy (gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(gameOver == true &&  Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}		 
+		
+	}
+	public void BirdDied()
+	{
+		gameOvertext.SetActive (true);
+		gameOver = true;
+	}
+
+	public void BirdScored()
+	{
+		if (gameOver)
+		{
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+	}
+	
+}
